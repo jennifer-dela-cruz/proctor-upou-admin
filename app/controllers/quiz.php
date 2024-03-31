d9fac0f80e82ad3edc0f67e9a31da77b2ec5c2f3
Jen Dela Cruz <purplejen@gmail.com>
2024-03-31 11:48:03 +0800

    Fixed the total violation count

--- app/controllers/quiz.php
+++ app/controllers/quiz.php
@@ -33,7 +33,15 @@ Class Quiz extends Controller
 				$data['attempt_data'] = $quiz->get_quiz_attempt_by_student($quizId, $studentId);
 				$data['quiz_data'] = $quiz->get_quiz($quizId);
 				$data['proctoring_type'] = $quiz->get_proctoring_type($quizId);
-				$data['total_violations'] = $quiz->get_total_violations($quizId, $studentId);
+
+				// Get violation count
+				$total_violations = 0;
+				if ($data['proctoring_type'][0]->proctoring_type == '1') {
+					$total_violations = count($data['attempt_data']);
+				} elseif ($data['proctoring_type'][0]->proctoring_type == '2') {
+					$data['total_violations'] = $quiz->get_total_violations($action, $id);
+					$total_violations = $data['total_violations'][0]->count;
+				}
 
 				// var_dump($data['attempt_data']);
 				// exit;
