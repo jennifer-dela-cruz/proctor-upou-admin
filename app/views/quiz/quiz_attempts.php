ce903e47ac7f378550241b46d079eb3e2df69502
Jen Dela Cruz <purplejen@gmail.com>
2024-03-31 10:28:47 +0800

    Added proctoring type value

--- app/views/quiz/quiz_attempts.php
+++ app/views/quiz/quiz_attempts.php
@@ -185,8 +185,15 @@ use Aws\Exception\AwsException;
 
 								<div class="col-12">
 									<span>
-									  <label class="form-label fw-semibold">Proctoring Type:</label>
-									  <label>Automated Proctoring</label>
+										<label class="form-label fw-semibold">Proctoring Type:</label>
+										<label>
+										<?=$data['proctoring_type']?>
+										<?php if($data['proctoring_type'] == '1'): ?>
+												<label>Automated proctoring</label>
+										<?php else if($data['proctoring_type'] == '2'): ?>
+												<label>Random snapshot</label>
+										<?php endif; ?>
+										</label>
 									</span>
 								</div>
 
